9db145fd681cd00a5c1649ed9058e5560fe8ac13
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-12-16 19:30:45 +0200

    posts

--- app/Views/post_screen.php
+++ app/Views/post_screen.php
@@ -8,7 +8,7 @@
   <h2><?= $post["post_name"] ?></h2>
   <img src="<?= $post["post_img"] ?>" alt="post_image" width="300px">
   <p><?= $post["content"] ?></p>
-
+  <p><a href="<?=base_url("posts")?>" class="btn btn-primary">Все новости</a></p>
 <?= $this->endSection() ?>
 
 
