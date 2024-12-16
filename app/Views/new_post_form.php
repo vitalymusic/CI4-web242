da4ba19aa95be47f6db474396f8699adcd1bda69
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-12-16 20:57:28 +0200

    added form

--- app/Views/new_post_form.php
+++ app/Views/new_post_form.php
@@ -6,8 +6,22 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-        <form action="">
-            
+        <form action="<?=base_url('save_post')?>" method="POST" enctype="multipart/form-data">
+        <div class="form-group mb-3">
+            <input type="text" class="form-control" id=""  placeholder="Post Name" name="post_name">
+        </div>
+        <div class="form-group mb-3">
+            <textarea class="form-control" id=""  name="content" placeholder="Post content"></textarea>
+        </div>
+        <div class="form-group mb-3">
+            <input type="file" class="form-control" id="" name="content" placeholder="Post image" name="post_img">
+        </div>
+
+        <input type="hidden" name="user_id" value="1">
+        <div class="form-group mb-3">
+            <input type="submit" value="Save post" class="btn btn-danger">
+        </div>
+
         </form>
 <?= $this->endSection() ?>
 
